1ef7b8bcd9f8e2236083cd9b32b982d82fa844c0
Arman Maurya <armanmarya6@gmail.com>
2024-12-29 16:22:24 +0530

    Fixed: border radius is not changing in editor view and navigation button overflows the container

--- src/ImageSlider.tsx
+++ src/ImageSlider.tsx
@@ -3,55 +3,90 @@ import { IoIosArrowForward } from "react-icons/io";
 import { IoIosPause } from "react-icons/io";
 import { IoIosPlay } from "react-icons/io";
 
-export const ImageSlider = ({ images, borderRadius }: { images: string[], borderRadius: string | number| undefined}) => {
-    const [currentIndex, setCurrentIndex] = useState(0);
-    const ref = useRef<HTMLImageElement>(null);
+export const ImageSlider = ({
+	images,
+	borderRadius,
+}: {
+	images: string[];
+	borderRadius: string | number | undefined;
+}) => {
+	const [currentIndex, setCurrentIndex] = useState(0);
+	const imageRef = useRef<HTMLImageElement>(null);
 
-    const next = () => {
-        if (currentIndex < images.length - 1) {
-            if (ref.current) {
-                if (ref.current.parentElement) {
-                    const allImages = ref.current.parentElement.querySelectorAll('img');
-                    allImages.forEach((image, i) => {
-                        // console.log(image);
-                        image.style.transform = `translateX(-${ (currentIndex + 1) * 100}%)`;
-                    })
-                }
-                
-            }
-            setCurrentIndex(currentIndex + 1);
-        }
-    }
+	const ImageContainerRef = useRef<HTMLDivElement>(null);
 
-    const prev = () => {
-        if (currentIndex >= 0) {
-            if (ref.current) {
-                if (ref.current.parentElement) {
-                    const allImages = ref.current.parentElement.querySelectorAll('img');
-                    allImages.forEach((image, i) => {
-                        // console.log(image);
-                        image.style.transform = `translateX(-${(currentIndex - 1) * 100}%)`;
-                    })
-                }
-                
-            }
-            setCurrentIndex(currentIndex - 1);
-        }
-    }
+	const next = () => {
+		if (currentIndex < images.length - 1) {
+			if (imageRef.current) {
+				if (imageRef.current.parentElement) {
+					const allImages =
+						imageRef.current.parentElement.querySelectorAll("img");
+					allImages.forEach((image, i) => {
+						// console.log(image);
+						image.style.transform = `translateX(-${
+							(currentIndex + 1) * 100
+						}%)`;
+					});
+				}
+			}
+			setCurrentIndex(currentIndex + 1);
+		}
+	};
 
+	const prev = () => {
+		if (currentIndex >= 0) {
+			if (imageRef.current) {
+				if (imageRef.current.parentElement) {
+					const allImages =
+						imageRef.current.parentElement.querySelectorAll("img");
+					allImages.forEach((image, i) => {
+						// console.log(image);
+						image.style.transform = `translateX(-${
+							(currentIndex - 1) * 100
+						}%)`;
+					});
+				}
+			}
+			setCurrentIndex(currentIndex - 1);
+		}
+	};
 
-    return (
-        <div style={{ position: 'relative', userSelect: "none"}}>
-            <div className="hover:cursor-pointer" style={{ position: "absolute", alignItems: 'center', justifyContent: 'center', display: 'flex', width: '100%', bottom: 0, gap: '20px', backgroundColor: 'rgba(5, 5, 5, 0.5)', zIndex: 10}}>
-                <IoIosArrowForward size={28} onClick={prev} color="black" style={{transform: 'rotate(180deg)'}}/>
-                <IoIosArrowForward size={28} onClick={next} className="hover:cursor-pointer"  color="black"/>
-            </div>
-            <div style={{ display: 'flex', flexDirection: 'row', overflowX: 'hidden', borderRadius:`${borderRadius}px`}}>
-                {images.map((image, i) => {
-                    return <img key={i} ref={ref} src={image} style={{ maxWidth: '100%', transition: "transform 0.5s ease-in-out"}} />
-                })}
-            </div>
-        </div>
-    )
-}
+	useEffect(() => {
+		if (ImageContainerRef.current) {
+			ImageContainerRef.current.style.borderRadius = `${borderRadius}px`;
+		}
+	}, []);
 
+	return (
+		<div className="image-slider-container" ref={ImageContainerRef}>
+			<div
+				className="image-slider-image-container"
+			>
+				{images.map((image, i) => {
+					return (
+						<img
+							className="image-slider-image"
+							key={i}
+							ref={imageRef}
+							src={image}
+						/>
+					);
+				})}
+			</div>
+            <div className="image-slider-nav-buttons-container">
+				<IoIosArrowForward
+					className="image-slider-nav-button-reverse image-slider-nav-button"
+					size={28}
+					onClick={prev}
+					color="black"
+				/>
+				<IoIosArrowForward
+					size={28}
+					onClick={next}
+					className="image-slider-nav-button"
+					color="black"
+				/>
+			</div>
+		</div>
+	);
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TFile } from 'obsidian';
+import {
+	App,
+	MarkdownView,
+	Modal,
+	Notice,
+	Plugin,
+	PluginSettingTab,
+	Setting,
+	TFile,
+} from "obsidian";
 // import {} from 'react-dom';
 // import React from 'react';
-import { createRoot } from 'react-dom/client';
+import { createRoot } from "react-dom/client";
 import { ImageSlider } from "./ImageSlider";
 
 interface ImageSliderSettings {
@@ -9,14 +18,15 @@ interface ImageSliderSettings {
 }
 
 const DEFAULT_SETTINGS: ImageSliderSettings = {
-	borderRadius: "10"
-}
+	borderRadius: "10",
+};
 
 export default class ImageSliderPlugin extends Plugin {
 	settings: ImageSliderSettings;
+	needRefresh: boolean = false;
 	async onload() {
 		await this.loadSettings();
-		this.registerMarkdownCodeBlockProcessor('slider', (source, el, ctx) => {
+		this.registerMarkdownCodeBlockProcessor("slider", (source, el, ctx) => {
 			// console.log('csv', source);
 			const extractImagePaths = (text: string) => {
 				// Regex patterns
@@ -24,12 +34,16 @@ export default class ImageSliderPlugin extends Plugin {
 				const markdownPattern = /\[.*?\]\((.*?)\)/g;
 
 				// Extract paths
-				const wikilinkPaths = [...text.matchAll(wikilinkPattern)].map(match => match[1]);
-				const markdownPaths = [...text.matchAll(markdownPattern)].map(match => match[1]);
+				const wikilinkPaths = [...text.matchAll(wikilinkPattern)].map(
+					(match) => match[1]
+				);
+				const markdownPaths = [...text.matchAll(markdownPattern)].map(
+					(match) => match[1]
+				);
 
 				// Combine results
 				return [...wikilinkPaths, ...markdownPaths];
-			}
+			};
 			const getResourcePaths = (imagePaths: string[]) => {
 				let imagesUri: string[] = [];
 				imagePaths.forEach((image) => {
@@ -39,11 +53,16 @@ export default class ImageSliderPlugin extends Plugin {
 					}
 				});
 				return imagesUri;
-			}
+			};
 			const parsedImages = extractImagePaths(source);
 			let imagesUri = getResourcePaths(parsedImages);
 			const root = createRoot(el);
-			root.render(<ImageSlider images={imagesUri} borderRadius={this.settings.borderRadius} />);
+			root.render(
+				<ImageSlider
+					images={imagesUri}
+					borderRadius={this.settings.borderRadius}
+				/>
+			);
 		});
 
 		// This adds a settings tab so the user can configure various aspects of the plugin
@@ -51,24 +70,45 @@ export default class ImageSliderPlugin extends Plugin {
 	}
 
 	async loadSettings() {
-
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign(
+			{},
+			DEFAULT_SETTINGS,
+			await this.loadData()
+		);
 	}
-
-
 
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
-}
 
+	public refreshOpenViews(): boolean {
+		try {
+			this.app.workspace.getLeavesOfType("markdown").forEach((leaf) => leaf.rebuildView());
+			return true;
+		} catch (e) {
+			console.error(e);
+			return false;
+		}
+	}
+}
 
 class ImageSliderSettingsTab extends PluginSettingTab {
 	plugin: ImageSliderPlugin;
+	needRefresh: boolean = false;
 
 	constructor(app: App, plugin: ImageSliderPlugin) {
 		super(app, plugin);
 		this.plugin = plugin;
+
+		app.setting.onClose = () => {
+			if (this.needRefresh) {
+				this.plugin.refreshOpenViews();
+			}
+			// this.plugin.refreshOpenViews();
+			app.setting.closeActiveTab();
+		}
+		// this.plugin.refreshOpenViews();
+		
 	}
 
 	display(): void {
@@ -76,17 +116,34 @@ class ImageSliderSettingsTab extends PluginSettingTab {
 		containerEl.empty();
 
 		new Setting(containerEl)
-			.setName('Border Radius')
-			.setDesc('Set the border radius for the images')
-			.addText(text => text
-				.setPlaceholder('Eg: 10')
-				.setValue(this.plugin.settings.borderRadius)
-				.onChange(async (value) => {
-					if (value) {
-						this.plugin.settings.borderRadius = value;
-						await this.plugin.saveSettings();
-						this.app.workspace.getActiveViewOfType(MarkdownView)?.previewMode.rerender(true);
-					}
-				}));
+			.setName("Border Radius")
+			.setDesc("Set the border radius for the images")
+			.addText((text) =>
+				text
+					.setPlaceholder("Eg: 10")
+					.setValue(this.plugin.settings.borderRadius)
+					.onChange(async (value) => {
+						if (value) {
+							if (this.plugin.settings.borderRadius !== value) {
+								this.needRefresh = true;
+							} else {
+								this.needRefresh = false;
+							}
+							this.plugin.settings.borderRadius = value;
+							await this.plugin.saveSettings();
+							// this.app.workspace.getActiveViewOfType(MarkdownView)?.previewMode.rerender(true);
+
+							// try {
+							// 	this.app.workspace.getLeavesOfType('markdown').forEach((leaf) => {
+							// 		leaf.rebuildView();
+							// 	})
+							// } catch (error) {
+							// 	new Notice('Error rebuilding views');
+							// }
+							// this.app.workspace.getActiveViewOfType(MarkdownView)?.
+							// this.app.workspace.getActiveViewOfType(editorEditorField)?.previewMode.rerender(true);
+						}
+					})
+			);
 	}
 }
